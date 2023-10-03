27a032ee7e1ae3aa188a68860d342d9835eec499
Mikhail Sermeev <m.sermeev@ roseltorg.ru>
2023-10-03 20:00:15 +0300

    Add groovy

---
+++ jenkins/autotests.groovy
@@ -0,0 +1,26 @@
+timeout(60){
+    node("maven"){
+        currentBuil.description= "BRANCH=${BRANCH}\nBASE_URL=${BASE_URL}"
+        stage("Checkout") {
+            checkout scm
+        }
+        stage("Running tests"){
+            def exitCode = sh (
+                returnStatus:true,
+                script:" mvn test -DBase.url=${BASE_URL}"
+            )
+            if(exitCode==1){
+                currentBild.result = 'UNSTABLE'
+            }
+        }
+        stage("Allure report"){
+            allure([
+                    invludeProperties:false,
+                    jdk:'',
+                    properties:[],
+                    reportBuildPolicy:'ALWAYS',
+                    results:[[path:'./target/allure-results']]
+            ])
+        }
+    }
+}
